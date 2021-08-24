fae82ef3f420c8de0725403ed780f75b09c8755b
Boba <bobaruljic@gmail.com>
2021-08-24 19:59:08 +0200

    zapocet DAO

--- VebProjekat/src/main/java/beans/Administrator.java
+++ VebProjekat/src/main/java/beans/Administrator.java
@@ -1,9 +1,20 @@
 package beans;
 
+import java.util.Date;
+
+import enums.Gender;
+import enums.Role;
+
 public class Administrator extends User {
 	
 	public Administrator() {
 		super();
+	}
+
+	
+	public Administrator(String username, String password, String fistName, String lastName, Gender gender, Date dateOfBirth,
+			Role role, boolean isDeleted, boolean isBlocked) {
+		super(username, password, fistName, lastName, gender, dateOfBirth, role, isDeleted, isBlocked);
 	}
 	
 }
---
+++ VebProjekat/src/main/java/dao/AdministratorDAO.java
@@ -0,0 +1,71 @@
+package dao;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import beans.Administrator;
+import enums.Gender;
+import enums.Role;
+
+public class AdministratorDAO {
+
+	private Gson gs = new Gson();
+	private HashMap<String, Administrator> admins = new HashMap<String, Administrator>(); // delete the initialization after creating the file!
+	private String path = "C:\\Users\\hp\\Desktop\\web-proj\\WebDelivery\\VebProjekat\\src\\main\\java\\data\\admins.json"; 
+
+	
+	public void loadAdmins() {
+		
+		// maybe we should add predefined values for boolean attributes, so we don't do this in constructors :(
+		admins.put("admin", new Administrator("admin", "admin", "Adam", "Martinez", Gender.MALE, new Date(), Role.ADMINISTRATOR, false, false));
+		
+		String json = gs.toJson(admins);
+		byte[] strInBytes = json.getBytes();
+
+		// writing in file for the first time; should be removed after the file is created
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(path);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			fos.write(strInBytes);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//----------------------------------------
+		
+		String adminsJson = "";
+		try {
+			adminsJson = new String(Files.readAllBytes(Paths.get(path)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Type type = new TypeToken<Map<String, Administrator>>() {}.getType();
+		admins.clear();
+		admins = gs.fromJson(adminsJson, type);
+		
+		//just to check it out 
+		for(Map.Entry<String, Administrator> entry : admins.entrySet()) {
+			System.out.println(entry.getValue().getUsername());
+		}
+	}
+}
+
