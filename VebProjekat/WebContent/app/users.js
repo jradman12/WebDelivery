60ab3743dcc0582f9c644ad956a1ca58ecc2e7cc
Boba <bobaruljic@gmail.com>
2021-08-30 18:07:27 +0200

    filter users table done

--- VebProjekat/WebContent/app/users.js
+++ VebProjekat/WebContent/app/users.js
@@ -11,18 +11,19 @@ let getUsers = new Vue({
 	el : "#usersiii",
 
 	data : {
-		//users :[{"username":"adam","password":"admin","fistName":"Adam","lastName":"Martinez","gender":"MALE","dateOfBirth":"Sep 24, 1992, 12:00:00 AM","role":"ADMINISTRATOR","isDeleted":false,"isBlocked":false}, {"username":"ella","password":"admin","fistName":"Ella","lastName":"Williams","gender":"FEMALE","dateOfBirth":"Mar 2, 1989, 12:00:00 AM","role":"ADMINISTRATOR","isDeleted":false,"isBlocked":true}, {"username":"mini","password":"minkica","fistName":"Minka","lastName":"Minkica","gender":"FEMALE","dateOfBirth":"Dec 12, 1999, 1:00:00 AM","role":"CUSTOMER","isDeleted":false,"isBlocked":false}],
+		    //users :[{"username":"adam","password":"admin","fistName":"Adam","lastName":"Martinez","gender":"MALE","dateOfBirth":"Sep 24, 1992, 12:00:00 AM","role":"DELIVERER","isDeleted":false,"isBlocked":false}, {"username":"ella","password":"admin","fistName":"Ella","lastName":"Williams","gender":"FEMALE","dateOfBirth":"Mar 2, 1989, 12:00:00 AM","role":"DELIVERER","isDeleted":false,"isBlocked":true}, {"username":"mini","password":"minkica","fistName":"Minka","lastName":"Minkica","gender":"FEMALE","dateOfBirth":"Dec 12, 1999, 1:00:00 AM","role":"CUSTOMER","isDeleted":false,"isBlocked":false}],
         users  : [],
         currentSort:'username',
         currentSortDir:'asc',
-        filter:''
+        filter:'',
+        roleFilter : ''
 	},
 
-    created : function() {
-        axios
-          .get('rest/users/getAllUsers')
-          .then(response => (this.users = fixData(response.data)))
-    },
+    // created : function() {
+    //     axios
+    //       .get('rest/users/getAllUsers')
+    //       .then(response => (this.users = fixData(response.data)))
+    // },
 
     methods : {
         sort:function(s) {
@@ -33,14 +34,21 @@ let getUsers = new Vue({
           }
         },
         computed:{
-            filteredUsers() {
+            filteredUsers: function() {
                 return this.users.filter(c => {
                   if(this.filter == '') return true;
                   return (c.username.toLowerCase().indexOf(this.filter.toLowerCase()) >= 0 || c.fistName.toLowerCase().indexOf(this.filter.toLowerCase()) >= 0 || c.lastName.toLowerCase().indexOf(this.filter.toLowerCase()) >= 0);
                 })
               },
+            filterRole() {
+              return this.filteredUsers.filter( x => {
+                if(!this.roleFilter) return true;
+                return (x.role === this.roleFilter);
+              })
+            },
             sortedUsers:function() {
-            return this.filteredUsers.sort((a,b) => {
+              let usss = !this.roleFilter ? this.filteredUsers : this.filterRole;
+              return usss.sort((a,b) => {
               let modifier = 1;
               if(this.currentSortDir === 'desc') modifier = -1;
               if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
