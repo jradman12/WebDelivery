654edcf25d3e7c2a56175d88575e6d343ead86ea
Jelena Radman <73402841+jradman12@users.noreply.github.com>
2022-02-01 22:26:59 +0100

    dodate validacije prilikom dodavanja novog korisnika

--- VebProjekat/WebContent/components/adminComponents/admin-addNewUser.js
+++ VebProjekat/WebContent/components/adminComponents/admin-addNewUser.js
@@ -143,56 +143,62 @@ Vue.component("admin-addNewUser", {
   methods: {
     addNewUser: function (event) {
       event.preventDefault();
-
-      if (this.newUser.type === "MANAGER") {
-        axios
-          .post("rest/managers/registration", {
-            fistName: this.newUser.fistName,
-            lastName: this.newUser.lastName,
-            dateOfBirth: this.newUser.dateOfBirth,
-            gender: this.newUser.gender,
-            username: this.newUser.username,
-            password: this.newUser.password,
-            restaurantID: null,
-            role: "MANAGER",
-          })
-          .then((response) => {
-            this.message = response.data;
-            toastr["success"]("Uspješno registrovan novi menadžer!");
-            window.location.assign(response.data);
-          })
-          .catch((err) => {
-            console.log("There has been an error! Please check this out: ");
-            console.log(err);
-          });
-      } else if (this.newUser.type === "DELIVERER") {
-        axios
-          .post("rest/deliverers/registration", {
-            fistName: this.newUser.fistName,
-            lastName: this.newUser.lastName,
-            dateOfBirth: this.newUser.dateOfBirth,
-            gender: this.newUser.gender,
-            username: this.newUser.username,
-            password: this.newUser.password,
-            role: "DELIVERER",
-          })
-          .then((response) => {
-            this.message = response.data;
-            toastr["success"]("Uspješno registrovan novi dostavljač!");
-            window.location.assign(response.data);
-          })
-          .catch((err) => {
-            console.log("There has been an error! Please check this out: ");
-            console.log(err);
-          });
-      }
-      document.getElementById("ime").value = "";
-      document.getElementById("prezime").value = "";
-      document.getElementById("datum").value = "";
-      document.getElementById("pol").value = "";
-      document.getElementById("tipUsera").value = "";
-      document.getElementById("korisnickoIme").value = "";
-      document.getElementById("lozinka").value = "";
-    },
+      console.log(this.newUser.password)
+      if(this.newUser.fistName == '' || this.newUser.lastName == '' || this.newUser.type == undefined 
+      || this.newUser.dateOfBirth == undefined || this.newUser.gender == undefined || this.newUser.username == '' || this.newUser.password == ''){
+        toastr["error"]("Sva polja moraju biti popunjena!");
+      }else{
+        if (this.newUser.type === "MANAGER") {
+          axios
+            .post("rest/managers/registration", {
+              fistName: this.newUser.fistName,
+              lastName: this.newUser.lastName,
+              dateOfBirth: this.newUser.dateOfBirth,
+              gender: this.newUser.gender,
+              username: this.newUser.username,
+              password: this.newUser.password,
+              restaurantID: null,
+              role: "MANAGER",
+            })
+            .then((response) => {
+              this.message = response.data;
+              toastr["success"]("Uspješno registrovan novi menadžer!");
+              window.location.assign(response.data);
+            })
+            .catch((err) => {
+              console.log("There has been an error! Please check this out: ");
+              console.log(err);
+            });
+        } else if (this.newUser.type === "DELIVERER") {
+          axios
+            .post("rest/deliverers/registration", {
+              fistName: this.newUser.fistName,
+              lastName: this.newUser.lastName,
+              dateOfBirth: this.newUser.dateOfBirth,
+              gender: this.newUser.gender,
+              username: this.newUser.username,
+              password: this.newUser.password,
+              role: "DELIVERER",
+            })
+            .then((response) => {
+              this.message = response.data;
+              toastr["success"]("Uspješno registrovan novi dostavljač!");
+              window.location.assign(response.data);
+            })
+            .catch((err) => {
+              console.log("There has been an error! Please check this out: ");
+              console.log(err);
+            });
+        }
+      
+        document.getElementById("ime").value = "";
+        document.getElementById("prezime").value = "";
+        document.getElementById("datum").value = "";
+        document.getElementById("pol").value = "";
+        document.getElementById("tipUsera").value = "";
+        document.getElementById("korisnickoIme").value = "";
+        document.getElementById("lozinka").value = "";
+    }
+    }
   },
 });
